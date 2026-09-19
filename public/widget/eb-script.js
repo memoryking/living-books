aad724fc5efae76ccf08e9602b8156f4c44573c7
memoryking <yhmh1130@gmail.com>
2026-09-19 17:47:00 +0900

    Fix content height: subtract page-bar height to prevent bottom clipping

--- public/widget/eb-script.js
+++ public/widget/eb-script.js
@@ -162,7 +162,8 @@
     var progEl=root.querySelector('.eb-progress');
     var barH=bar?bar.offsetHeight:0;
     var progH=progEl?progEl.offsetHeight:0;
-    var navH=pageNav?pageNav.offsetHeight:48;
+    var pageBar=document.getElementById('eb-page-bar');
+    var navH=pageBar?pageBar.offsetHeight:40;
     var viewH=(inIframe&&parentViewportHeight>0)?parentViewportHeight:window.innerHeight;
     contentHeight=viewH-barH-progH-navH;
     if(contentHeight<200)contentHeight=200;
